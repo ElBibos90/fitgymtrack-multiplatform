3a441752a305d6d73ec03e36d886522bc7908b11
ElBibos90 <eddy.rebes@gmail.com>
2025-05-30 21:59:36 +0200

    sistemato errore

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -29,7 +29,9 @@ kotlin {
                 implementation("io.ktor:ktor-client-okhttp:2.3.9")
                 implementation("io.ktor:ktor-client-logging:2.3.9")
                 implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.9")
-
+				
+				implementation("io.ktor:ktor-client-auth:2.3.9")                    
+				implementation("io.ktor:ktor-client-content-negotiation:2.3.9") 
 
 
                 implementation("org.jetbrains.compose.runtime:runtime:1.7.3")
